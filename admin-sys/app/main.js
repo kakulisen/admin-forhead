fb419bee397be05c8f4e25c4d6ffa130e6150183
kakulisen <992825851@qq.com>
2018-04-12 18:52:38 +0800

    first commit

---
+++ admin-sys/app/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import './main.css';
+
+render(<Greeter />, document.getElementById('root'));
+
+
+//const greeter = require('./Greeter.js');
+//document.querySelector('#root').appendChild(greeter());
